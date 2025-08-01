7be2b92e913e95fc9c0ac804191d1e6b7fcc29b2
Shashank Sinha <shashanksinha590@gmail.com>
2025-08-02 00:42:10 +0530

    branch issue

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        Debug.Log("Test");
+        Debug.Log("Check");
     }
     void Start()
     {
