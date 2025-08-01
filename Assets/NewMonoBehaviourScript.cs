9b3f85ac8a035203a00967795af2b605b07981b5
Shashank Sinha <shashanksinha590@gmail.com>
2025-08-02 00:44:17 +0530

    resolved

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -2,11 +2,6 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
-    {
-        Debug.Log("Check");
-    }
     void Start()
     {
         
